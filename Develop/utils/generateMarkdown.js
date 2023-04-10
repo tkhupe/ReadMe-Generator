d75664a60ae7068ae4b3c1ddf27ce0ea7c7d1be4
Tshakalisa Khupe <tshakalisakhupe@Tshakalisas-Air.lan>
2023-04-10 12:38:08 -0500

    added return statement for remderLicenseSection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -26,7 +26,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `  
+  ## License
+  ${renderLicenseBadge(license)}
+  <br />
+  ${renderLicenseLink(license)}
+
+  This application is covered by the ${license} license. 
+  `;
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
