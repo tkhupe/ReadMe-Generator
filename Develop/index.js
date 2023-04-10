7a7e3cf4c0e55f38fb653ca88ab301f5bcaf1df5
Tshakalisa Khupe <tshakalisakhupe@Tshakalisas-Air.lan>
2023-04-10 12:28:35 -0500

    added functions to utils and index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,95 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type:"input",
+    name:"projectTitle",
+    message:"What is the project title?"
+},
+{
+    type: "input",
+    name:"description",
+    message:"Write a brief description of the project:"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Describe the installation of the project:"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Write a short description of how your project will be used:"
+},
+{
+    type: "list",
+    name: "license",
+    message: "Add a license to your project",
+    choices: [
+        "Apache",
+        "MIT License",
+        "opensource.org/lic",
+        "Mozilla Public License",
+        "Academic",
+        "ISC Foundation",
+        "GNU"
+    ]
+},
+{
+    type: "input",
+    name: "contributors",
+    message: "List of contributors:"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Are there any tests included?"
+},
+{
+    type: "input",
+    name: "questions",
+    message: "What do I do to solve an issue?"
+},
+{
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username:"
+
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please enter your email address"
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, () => {});
+
+} 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   
+    inquirer.prompt(questions)
+    .then((answers) => {
+        let data = generateMarkdown(answers);
+        writeToFile(
+            './README.md',data
+        )
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === "Apache") return `![badge](https://img.shields.io/badge/License-Apache_2.0-blue.svg)<br />`;
+  if (license === "MIT License") return `![badge](https://img.shields.io/badge/License-MIT-yellow.svg)<br />`;
+  if (license === "Open Data Commons License") return `![badge](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)<br />`;
+  if (license === "Mozilla Public License") return `![badge](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)<br />`;
+  if (license === "GNU") return `![badge](https://img.shields.io/badge/License-GPLv3-blue.svg)<br />`;
+  if (license ===  "ISC Foundation") return `![badge](https://img.shields.io/badge/License-ISC-blue.svg)<br />`;
+  if (license === "") return '';
+  
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Apache") return '(https://opensource.org/licenses/Apache-2.0)';
+  if (license === "MIT License") return '(https://opensource.org/licenses/MIT)';
+  if (license === "Open Data Commons License") return '(https://opendatacommons.org/licenses/by/)';
+  if (license === "Mozilla Public License") return '(https://opensource.org/licenses/MPL-2.0)';
+  if (license === "GNU") return 'https://www.gnu.org/licenses/gpl-3.0)';
+  if (license ===  "ISC Foundation") return '(https://opensource.org/licenses/ISC)';
+  if (license === "") return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `  
+  ## License
+  ${renderLicenseBadge(license)}
+  <br />
+  ${renderLicenseLink(license)}
+
+  This application is covered by the ${license} license. 
+  `;
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
+  return `
+  <h1 align="center">${data.projectTitle}</h1>
+  
+  ${renderLicenseBadge(data.license)}
+  
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ✏️ ${data.tests}
+  
+  ## Questions
+  ${data.questions}<br />
+  <br />
+  Find me on GitHub: [${data.username}](https://github.com/${data.username})<br />
+  <br />
+  ✉️ Email me with any questions: ${data.email}<br /><br />`;
+    }
 module.exports = generateMarkdown;
